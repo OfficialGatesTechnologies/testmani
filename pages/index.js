90267268d64df2049d3a7fe321f9208dde9331a1
OfficialGates <gunasekar.k@officialgates.com>
2019-12-27 17:43:34 +0530

    new

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react';
 import Head from 'next/head';
 import { site_name } from '../utils/Common';
 import { withRouter } from 'next/router';
-
+import axios from 'axios';
+import io from 'socket.io-client';
 export default withRouter(class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          message :''
+        }
+    }
     componentDidMount = () => {
+        this.socket = io();
+        setInterval(this.receiveValues(), 1000)
+    }
+    receiveValues = () => {
+        this.socket.on('connectUserFront', (req) => {
+            this.setState({ message: req.will });
+            console.log('connectUser front end'); // true
+        });
+    }
+    shoot = () => {
+        this.socket.emit('connectProvider',{value:'connected from react'});
     }
     render() {
         return (
@@ -15,13 +33,14 @@ export default withRouter(class Index extends Component {
                     <title>{site_name} | Home</title>
                     <meta name="distribution" content="Global" />
                     <meta name="revisit-after" content="1 days" />
-                    
+
                 </Head>
- 
+
                 <div className="main-wrap">
-                    <p>Body</p>
+                    <p>Body - {this.state.message}</p>
+                    <button onClick={this.shoot}>Take the shot!</button>
                 </div>
- 
+
             </div>
         )
     }
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const socketIO = require('socket.io');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3002;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const path = require('path')
+const http = require('http');
 const compression = require('compression')
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare()
     .then(() => {
-        const server = express()
-        server.use(compression())
-        server.use(handler);
-        server.use(function (req, res, next) {
+        const nextapp = express();
+        var server = http.createServer(nextapp);
+        var io = socketIO(server);
+        nextapp.use(compression());
+        nextapp.use(handler);
+        nextapp.use(function (req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
                 const secureUrl = "https://" + req.headers['host'] + req.url;
                 res.writeHead(301, { "Location": secureUrl });
@@ -22,9 +28,20 @@ app.prepare()
             }
             next();
         });
-        server.get('*', (req, res) => {
+        nextapp.get('*', (req, res) => {
             return handle(req, res)
         })
+        io.on('connection', (socket) => {
+           
+            console.log(' Connected');
+            socket.on('connectUser', (params) => {
+                console.log(' connectUser');
+            });
+            socket.on('connectProvider', (params) => {
+                console.log(' params', params);
+                io.emit('connectUserFront', { will: params.value });
+            });
+        });
         server.listen(port, (err) => {
             if (err) throw err;
             console.log(`Listening on port ${port}...`);
