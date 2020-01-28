5c28d73a76622e0b5befd9fa3079bf0e3dc3acd1
OfficialGates <gunasekar.k@officialgates.com>
2020-01-28 17:19:37 +0530

    loading
    loading

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { withRouter } from 'next/router';
 import axios from 'axios';
 import io from 'socket.io-client';
 import firebase from 'firebase';
+import ContentLoader from 'react-content-loader'
 export default withRouter(class Index extends Component {
     constructor(props) {
 
@@ -35,6 +36,9 @@ export default withRouter(class Index extends Component {
                 .then(registration => {
                     console.log('service worker registration successful')
                     this.state.messaging.useServiceWorker(registration);
+                     this.state.messaging.setBackgroundMessageHandler(function (payload) {
+            console.log('Message received: ', payload);
+        });
                 }).then(() => {
 
                 })
@@ -42,6 +46,7 @@ export default withRouter(class Index extends Component {
                     console.warn('service worker registration failed', err.message)
                 })
         }
+       
         this.socket = io();
         // setInterval(this.receiveValues(), 1000);
         this.receiveValues();
@@ -69,6 +74,7 @@ export default withRouter(class Index extends Component {
             console.log('FCM_TOKEN:', token);
         });
 
+
     }
 
     render() {
@@ -83,6 +89,48 @@ export default withRouter(class Index extends Component {
                 </Head>
 
                 <div className="main-wrap">
+                    <ContentLoader
+                        viewBox="0 0 1360 300"
+                        height={300}
+                        width={1360}
+                        speed={2}
+                        
+                    >
+                        <rect x="30" y="20" rx="8" ry="8" width="200" height="200" />
+                        <rect x="30" y="250" rx="0" ry="0" width="200" height="18" />
+                        <rect x="30" y="275" rx="0" ry="0" width="120" height="20" />
+                        <rect x="250" y="20" rx="8" ry="8" width="200" height="200" />
+                        <rect x="250" y="250" rx="0" ry="0" width="200" height="18" />
+                        <rect x="250" y="275" rx="0" ry="0" width="120" height="20" />
+                        <rect x="470" y="20" rx="8" ry="8" width="200" height="200" />
+                        <rect x="470" y="250" rx="0" ry="0" width="200" height="18" />
+                        <rect x="470" y="275" rx="0" ry="0" width="120" height="20" />
+                        <rect x="690" y="20" rx="8" ry="8" width="200" height="200" />
+                        <rect x="690" y="250" rx="0" ry="0" width="200" height="18" />
+                        <rect x="690" y="275" rx="0" ry="0" width="120" height="20" />
+                        
+                     
+                    </ContentLoader>
+ 
+                    <ContentLoader 
+                        speed={2}
+                        width={600}
+                        height={160}
+                        viewBox="0 0 600 160"
+                        backgroundColor="#f3f3f3"
+                        foregroundColor="#ecebeb"
+                    >
+                        <circle cx="10" cy="20" r="8" /> 
+                        <rect x="25" y="15" rx="5" ry="5" width="220" height="10" /> 
+                        <circle cx="10" cy="50" r="8" /> 
+                        <rect x="25" y="45" rx="5" ry="5" width="220" height="10" /> 
+                        <circle cx="10" cy="80" r="8" /> 
+                        <rect x="25" y="75" rx="5" ry="5" width="220" height="10" /> 
+                        <circle cx="10" cy="110" r="8" /> 
+                        <rect x="25" y="105" rx="5" ry="5" width="220" height="10" />
+                    </ContentLoader>
+ 
+                    
                     <p>Body - {this.state.message}</p>
                     <button onClick={this.shoot}>Take the shot!</button>
                     <button onClick={this.getFCMToken}>Take the shot!</button>
