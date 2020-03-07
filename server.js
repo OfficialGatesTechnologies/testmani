91e1689e991ece6cf6e6756b7102515329e934c3
OfficialGates <gunasekar.k@officialgates.com>
2020-03-07 12:03:14 +0530

    tet
    tsetet

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const path = require('path')
@@ -10,6 +10,63 @@ const http = require('http');
 const compression = require('compression')
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
+
+// https://trinitytuts.com/tips/deploying-a-nextjs-app-in-production-with-pm2/
+// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
+
+// >>> curl - o - https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+// >>> . ~/.nvm/nvm.sh
+
+//     >>> nvm install 10.16.3
+
+//         >>> node - e "console.log('Running Node.js ' + process.version)"
+
+
+// https://www.npmjs.com/package/pm2
+
+// >>> npm install pm2 - g
+
+
+// cd /var/www/html / node - apis
+
+// npm install
+
+//     >>> cd /var/www/html / socket
+//         >>> node server.js
+//             >>> npm install pm2 - g
+//                 >>> pm2 start server.js
+//                     >>> pm2 restart server.js
+//                         >>> pm2 stop server.js
+
+
+// Security group AWS port enable
+// Custom
+// TCP
+// 3000
+
+
+// =======================REMOVE Port==================================
+
+// <VirtualHost *:80>
+//     ServerAdmin root@localhost
+//     ServerName api.docdit.com
+//     ProxyPreserveHost On
+
+//     # setup the proxy
+//     <Proxy *>
+//         Order allow,deny
+//         Allow from all
+//     </Proxy>
+//     ProxyPass / http://3.136.196.122:8080/
+//     ProxyPassReverse / http://3.136.196.122:8080/
+// </VirtualHost>
+
+// <VirtualHost *:80>
+// ServerName docdit.com
+// DocumentRoot /var/www/html
+// </VirtualHost>
+
 
 app.prepare()
     .then(() => {
